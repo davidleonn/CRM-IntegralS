cb3bc171a0782ccd3e811a32767afaf615f2c08c
David León <davidleon_06@hotmail.com>
2022-07-06 18:01:03 +0200

    Revert "r"
    This reverts commit b04202b71fffc914af2a944590f3efdae9784722.

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET dashboard page. */
 router.get('/', function(req, res, next) {
-  res.render('/dashboard/*', { title: 'Express' });
+  res.render('dashboard', { title: 'Express' });
 });
 
 module.exports = router;
