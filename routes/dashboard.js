9a33dec819604af1872ccda81bc2ec01ab1ee5c2
Harsh <84454566+Har99sh@users.noreply.github.com>
2022-07-25 15:00:00 +0200

    new dashboard setup and routes

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -8,29 +8,26 @@ router.get('/', function(req, res, next) {
 
 /* GET dashboard page. */
 router.get('/enter-data/expenses', function(req, res, next) {
-  res.render('expenses');
+  res.render('../views/crm/expensesForm');
 });
 
 /* GET sales page. */
 router.get('/enter-data/sales', function(req, res, next) {
-  res.render('sales');
+  res.render('../views/crm/salesForm');
 });
 
 router.get('/show-data/sales', function(req, res, next) {
-  res.render('showdatasales');
+  res.render('../views/crm/salesReport');
 })
 
 
 router.get('/show-data/expenses', function(req, res, next) {
-  res.render('showdataexpenses');
+  res.render('../views/crm/expensesReport');
 })
 
-router.get('/show-data/summary', function(req, res, next) {
-  res.render('showdatasummary');
-})
 
 router.get('/contact', function(req, res, next) {
-  res.render('contact');
+  res.render('../views/crm/salesForm');
 });
 
 module.exports = router;
