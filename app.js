0623da7870c5392a57345963a41630afa50eaf46
Harsh <84454566+Har99sh@users.noreply.github.com>
2022-07-27 10:12:26 +0200

    test chart for dashbnaord + chart.js config

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use(express.static(__dirname + '/node_modules/chart.js/dist'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
