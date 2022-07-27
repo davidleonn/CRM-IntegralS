5ba0bef14fa478234e6388811b3a2fa69af11e45
Harsh <84454566+Har99sh@users.noreply.github.com>
2022-07-27 22:00:36 +0200

    sign out, clear cookies

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/signout', (req, res) =>{
+  res.clearCookie("userId");
+  res.clearCookie("companyId");
+  res.redirect('/')
+})
 module.exports = router;
